92b6959fa5b2b9389476ccbbebc14505105ccff1
marcus david Bronstein <marcusdb@gmail.com>
2017-07-11 18:51:44 -0300

    working

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import Demo from './MarbleStream'
 
 
 const App = (props) => (
-
   <div className="slides">
-
-    <section>Fun with Functional Reactive Programming<Demo/></section>
+  <section data-background="img/zwift-screen-watopia-2.jpg">
+<img src="img/zwift-primary-color.png" style={{width:'200px',backgroundColor:'white'}}/>
+<h2 className="shadow">Tech Talk & Happy Hour</h2>
+</section>
+    <section><h2>Fun with Functional Reactive Programming</h2>
+					<h4>Marcus David Bronstein @marcusdb</h4>
+          </section>
     <section>
-      <section>Vertical Slide 1</section>
+      <section>fun</section>
+      <section><Demo/></section>
       <section data-state="slideA"><SlideRenderer trigger="slideA"><img src="img/mind-blown.gif"/></SlideRenderer></section>
     </section>
   </div>
