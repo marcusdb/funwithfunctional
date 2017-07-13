a98997560604349d417effc1952337571b9f8cd3
marcus david Bronstein <marcusdb@gmail.com>
2017-07-13 08:06:22 -0300

    more examples

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import Example4 from './Example4'
 import Example5 from './Example5'
 import Example6 from './Example6'
 import Example7 from './Example7'
-import Example8 from './Example8'
+import Example9 from './Example9'
+import Example10 from './Example10'
+import Example11 from './Example11'
 
 render(<Example3/>, document.getElementById('example3'))
 render(<Example4/>, document.getElementById('example4'))
 render(<Example5/>, document.getElementById('example5'))
 render(<Example6/>, document.getElementById('example6'))
 render(<Example7/>, document.getElementById('example7'))
-//render(<Example8/>, document.getElementById('example8'))
+render(<Example9/>, document.getElementById('example9'))
+render(<Example10/>, document.getElementById('example10'))
+render(<Example11/>, document.getElementById('example11'))
---
+++ src/Example10.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import Rx from 'rxjs';
+import MarbleStream from './MarbleStream'
+
+const matcher=(val)=>{
+  let target = "marcus"
+  let regex=new RegExp('('+val+')');
+  return target.match(regex)?target.match(regex)[0]: '';
+}
+
+function randomDelay(bottom, top) {
+  return Math.floor( Math.random() * ( 1 + top - bottom ) ) + bottom;
+}
+
+const remoteCall=(args)=>(
+  Rx.Observable
+      .of(matcher(args))
+      .delay(randomDelay(10,1000))
+)
+
+class Example10 extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      stream1: {},
+      stream2: {},
+      value:''
+    }
+    this.draw.bind(this)
+    this.updateResult.bind(this)
+
+  }
+
+  draw(value, name) {
+    this.setState((prevState) => {
+      return {
+        [name]: {
+          value,
+          id: parseInt(Math.random() * 10000)
+        }
+      };
+    })
+  }
+
+  updateResult(result){
+    this.setState({value:result})
+  }
+
+  componentDidMount() {
+    let input$ = Rx.Observable.fromEvent(document.getElementById('example10_input1'), 'keyup')
+    .map((evt) => evt.target.value).do((value) => this.draw(value, 'stream1'));
+
+    input$.switchMap(val=>remoteCall(val)).do((value) => this.draw(value, 'stream2')).subscribe((value)=>this.updateResult(value))
+
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>stream de eventos</h3>
+        <input id='example10_input1' style={{
+          width: '150px',
+          height: '30px',
+          fontSize: '24px'
+        }}/>
+        &nbsp;Resultado&nbsp;
+        --<span>{this.state.value}</span>--
+        <pre><code is className="javascript hljs" data-trim contenteditable>
+          {`
+            let input$ = Rx.Observable.fromEvent(document.getElementById('example10_input1'), 'keyup')
+            .map((evt) => evt.target.value);
+            input$.switchMap(val=>remoteCall(val)).subscribe(this.updateResult)
+`}
+        </code></pre>
+        <MarbleStream velocity={80} offset={50} distance={900} marble={this.state.stream1}/>
+        <MarbleStream velocity={80} offset={50} distance={900} marble={this.state.stream2}/>
+
+      </div>
+    )
+  }
+}
+
+export default Example10
