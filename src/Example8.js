89c117ac0ecbfc560055d8df76528f033be80e75
marcus david Bronstein <marcusdb@gmail.com>
2017-07-13 00:02:25 -0300

    working

--- src/Example8.js
+++ src/Example8.js
@@ -4,75 +4,33 @@ import MarbleStream from './MarbleStream'
 
 const isNumeric = (val) => (!isNaN(parseFloat(val)) && isFinite(val))
 
+const getOffset = (el) => {
+  return {left: el.offsetLeft, top: el.offsetTop}
+  //return el.getBoundingClientRect();
+}
+
 class Example8 extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      stream1: {},
-      stream2: {},
-      stream3: {}
-    }
-    this.draw.bind(this)
 
-  }
-
-  draw(value, name) {
-    this.setState((prevState) => {
-      return {
-        [name]: {
-          value,
-          id: parseInt(Math.random() * 10000)
-        }
-      };
-    })
-
-  }
-
-  componentDidMount() {
-    const element = document.getElementById('dragTarget')
-    //let mouseup$ = Rx.Observable.fromEvent(element, 'mouseup').do((value) => this.draw(value, 'stream1')).subscribe()
-    let mousemove$ = Rx.Observable.fromEvent(element, 'mousemove').do((value) => this.draw(value, 'stream2')).subscribe()
-    // let mousedown$ = Rx.Observable.fromEvent(element, 'mousedown').do((value) => this.draw(value, 'stream3')).map(event => {
-    //   event.preventDefault();
-    //   return {
-    //     left: event.clientX - element.offset().left,
-    //     top: event.clientY - element.offset().top
-    //   };
-    // }).do((value) => this.draw(value, 'stream3'))
-    //
-    // let mousedrag$ = mousedown$.mergeMap((offsetData) => mousemove$.map((position) => ({
-    //   left: position.clientX - offsetData.left,
-    //   top: position.clientY - offsetData.top
-    // })).takeUntil(mouseup$))
-
-    //mousedrag$.subscribe((resultPosition) =>(element.style={...element.style,top: pos.top, left: pos.left})); 				// Update position
-    //mousedrag$.subscribe(console.log); 				// Update position
-
-  }
 
   render() {
     return (
       <div>
-        <h6>stream de eventos</h6>
+        Drag And Drop
         <pre><code is className="javascript hljs" data-trim contenteditable>
           {`
-            let plus$ = Rx.Observable.fromEvent(element, 'keyup')
-            .map((evt) => evt.target.value).filter(val=>isNumeric(val))
-            .map(val=>parseFloat(val)).startWith(0)
-
-
-            let minus$ = Rx.Observable.fromEvent(element, 'keyup')
-            .map((evt) => evt.target.value).filter(val=>isNumeric(val))
-            .map(val=>parseFloat(val)).startWith(0)
-
-
-            minus$.combineLatest(plus$,(s1,s2)=>s1+s2).subscribe(updateResultValue);
+            let mouseup$ = Rx.Observable.fromEvent(element, 'mouseup')
+            let mousemove$ = Rx.Observable.fromEvent(element, 'mousemove')
+            let mousedown$ = Rx.Observable.fromEvent(element, 'mousedown').map(event => getOffset(event))
+            let mousedrag$ = mousedown$.mergeMap((offsetData) =>
+                mousemove$.map((position) => (calculateNewPositionWithOffset(offsetData,position)))
+                .takeUntil(mouseup$)
+            )
+            mousedrag$.subscribe((resultPosition) => this.updatePosition(resultPosition));
 `}
         </code></pre>
-        <MarbleStream velocity={80} offset={50} distance={900} marble={this.state.stream1}/>
-        <MarbleStream velocity={80} offset={50} distance={900} marble={this.state.stream2}/>
-        <MarbleStream velocity={80} offset={50} distance={900} marble={this.state.stream3}/>
-        <div id='dragTarget' style={{backgroundColor:'red'}}>DRAG HERE</div>
+        <aside className="notes">
+          code pen https://codepen.io/marcusdb/pen/GEwYZd plain https://codepen.io/marcusdb/pen/rwPOEJ
+        </aside>
 
       </div>
     )
