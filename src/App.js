5ae1783960915753fdfafd9665f16ab2d68422fb
marcus david Bronstein <marcusdb@gmail.com>
2017-07-13 11:08:10 -0300

    example12

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Example7 from './Example7'
 import Example9 from './Example9'
 import Example10 from './Example10'
 import Example11 from './Example11'
+import Example12 from './Example12'
 
 render(<Example3/>, document.getElementById('example3'))
 render(<Example4/>, document.getElementById('example4'))
@@ -18,3 +19,4 @@ render(<Example7/>, document.getElementById('example7'))
 render(<Example9/>, document.getElementById('example9'))
 render(<Example10/>, document.getElementById('example10'))
 render(<Example11/>, document.getElementById('example11'))
+render(<Example12/>, document.getElementById('example12'))
---
+++ src/Example12.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import Rx from 'rxjs';
+import MarbleStream from './MarbleStream'
+
+class Example12 extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      stream1: {},
+      stream2: {},
+      stream3: {}
+    }
+    this.draw.bind(this)
+
+  }
+
+  draw(value, name) {
+    this.setState({
+        [name]: {
+          value,
+          id: parseInt(Math.random() * 10000)
+        }
+      })
+
+  }
+
+  componentDidMount() {
+    let clicks$ = Rx.Observable.fromEvent(document.getElementById('example12_btn1'), 'click')
+
+    clicks$
+    .buffer(clicks$.debounceTime(400))
+    .map((list) => list.length)
+    .do((value) => this.draw(value, 'stream1'))
+    .filter(x=>x>2)
+    .do((value) => this.draw(value, 'stream2'))
+    .subscribe();
+
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>stream de eventos</h3>
+        <button id='example12_btn1' style={{
+          width: '120px',
+          height: '30px',
+          fontSize: '24px'
+        }}>click me</button>
+
+        <pre><code is className="javascript hljs" data-trim contenteditable>
+          {`
+            let clicks$ = Rx.Observable.fromEvent(document.getElementById('example12_btn1'), 'click')
+
+            clicks$
+            .buffer(clicks$.debounceTime(400))
+            .map((list) => list.length)
+            .filter(x=>x>2)
+            .subscribe();
+`}
+        </code></pre>
+        <MarbleStream velocity={80} offset={50} distance={900} marble={this.state.stream1}/>
+        <MarbleStream velocity={80} offset={50} distance={900} marble={this.state.stream2}/>
+
+      </div>
+    )
+  }
+}
+
+export default Example12
